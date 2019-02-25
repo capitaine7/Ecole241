335469b5d0f8f4336a2149469c84765eeae795fb
capitaine7 <38183440+capitaine7@users.noreply.github.com>
2019-02-25 12:09:54 +0100

    mise a jour

--- sprit/s9.js
+++ sprit/s9.js
@@ -1,6 +1,6 @@
-var nombr1 = prompt("saisir un, nombre 1"); 
-var nombr1 = prompt("saisir un, nombre 2");
-var nombr1 = prompt("saisir un, nombre 3");
-var nombr1 = prompt("saisir un, nombre 4");
-var nombr1 = prompt("saisir un, nombre 5");
+var nombr1 = prompt("saisir un nombre 1"); 
+var nombr1 = prompt("saisir un nombre 2");
+var nombr1 = prompt("saisir un nombre 3");
+var nombr1 = prompt("saisir un nombre 4");
+var nombr1 = prompt("saisir un nombre 5");
 	console.log("le plus grand nombre est:" + Math.max(nombr1,nombr2,nombr3,nombr4,nombr5));
