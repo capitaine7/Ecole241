b603131b8e7668cd681c7760d22f1de403fd010a
capitaine7 <didcapitaine@gmail.com>
2019-03-27 11:07:49 +0100

    misa a jour

--- sprint/php/act3.php
+++ sprint/php/act3.php
@@ -1,5 +1,5 @@
 <?php
 $okaclassroom = array("noël","ismael","sissou","yvan","francois","hamilton","grace","axel","falana","ebaneth","Din","maurio","les autres");
 
- echo " $okaclassroom[0], $okaclassroom[1], $okaclassroom[2], $okaclassroom[3], $okaclassroom[4], $okaclassroom[5], $okaclassroom[6], $okaclassroom[7], $okaclassroom[8], $okaclassroom[9], $okaclassroom[10], $okaclassroom[11], et $okaclassroom[12]; sont des apprenants de l'ecole 241";
+ echo " $okaclassroom[0], $okaclassroom[1], $okaclassroom[2], $okaclassroom[3], $okaclassroom[4], $okaclassroom[5], $okaclassroom[6], $okaclassroom[7], $okaclassroom[8], $okaclassroom[9], $okaclassroom[10], $okaclassroom[11], et $okaclassroom[12]; sont des apprenants de l'ecole 241 ce sont des codeurs en réalité";
 ?>
