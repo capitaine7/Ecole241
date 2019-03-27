2b272bf19c6688eed8b3354068c3361cbbd6267b
capitaine7 <didcapitaine@gmail.com>
2019-03-27 10:36:18 +0100

    misa a jour

---
+++ sprint/php/act3.php
@@ -0,0 +1,5 @@
+<?php
+$okaclassroom = array("noël","ismael","sissou","yvan","francois","hamilton","grace","axel","falana","ebaneth","Din","maurio","les autres");
+
+ echo " $okaclassroom[0], $okaclassroom[1], $okaclassroom[2], $okaclassroom[3], $okaclassroom[4], $okaclassroom[5], $okaclassroom[6], $okaclassroom[7], $okaclassroom[8], $okaclassroom[9], $okaclassroom[10], $okaclassroom[11], $okaclassroom[12]; sommes des apprenants de l'ecole 241";
+?>
